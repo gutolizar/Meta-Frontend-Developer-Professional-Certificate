0ebeada2846b6371b860586e6c0c5f2287bfba0e
Luiz Alves <113264157+gutolizar@users.noreply.github.com>
2023-01-30 09:24:44 -0300

    Lab create import
    Here i had to take the following steps:
    Move the Heading function from App.js to a separate component file, named “Heading.js”. 
    Import the Heading component into the App component after creating it.

---
+++ #5lab_createimport.jsx
@@ -0,0 +1,21 @@
+import Heading from "./Heading";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
+
+// The heading component below
+
+function Heading() {
+  return (
+    <h1></h1>
+  )
+}
+
+export default Heading;
